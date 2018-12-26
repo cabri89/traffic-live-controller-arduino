bb61890ada6e4835b88877df930f7fe38a7c453c
cabri <cabridens@gmail.com>
2018-12-26 23:05:31 +0100

    Ajout du readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # traffic-live-controller-arduino
+
+Permet de controller un feu tricolor à l'aide de :
+- 3 relays qui sont reliés aux ampoules du feu,
+- 3 switchs pour contrôler chaque ampoule via l'Arduino,
+- 2 push buttons, 1 pour changer de mode et l'autre pour changer la vitesse.  
--- feu_tricolor.ino
+++ feu_tricolor.ino
@@ -1,14 +1,14 @@
 
-int redSwitch = 3; 
+int redSwitch = 3;
 int orangeSwitch = 4;
-int greenSwitch = 2;   
-     
-int red = 10;  
-int orange = 9;      
+int greenSwitch = 2;
+
+int red = 10;
+int orange = 9;
 int green = 11;
 
 int delayButton = 6;
-int modeButton = 7; 
+int modeButton = 7;
 
 int onState = 1;
 int offState = 0;
@@ -39,110 +39,136 @@ void setup()
   pinMode(orangeSwitch, OUTPUT);
   pinMode(greenSwitch, OUTPUT);
   pinMode(modeButton, OUTPUT);
-  
+
   pinMode(red, OUTPUT);
   pinMode(orange, OUTPUT);
   pinMode(green, OUTPUT);
 }
 
-int readModeButton(){
-   readingMode = digitalRead(modeButton);
-  
-  if (readingMode != previousMode) {
-     if(readingMode == offState){
-        if(numMode == totalMode){
-          numMode = 1;
-        }else{
-          numMode++;
-        } 
-      }   
-      multDelay = 1;  
+int readModeButton()
+{
+  readingMode = digitalRead(modeButton);
+
+  if (readingMode != previousMode)
+  {
+    if (readingMode == offState)
+    {
+      if (numMode == totalMode)
+      {
+        numMode = 1;
+      }
+      else
+      {
+        numMode++;
+      }
+    }
+    multDelay = 1;
   }
-  
+
   previousMode = readingMode;
 }
 
-int readDelayButton(){
+int readDelayButton()
+{
   readingDelay = digitalRead(delayButton);
-  
-  if (readingDelay != previousDelay) {
-     if(readingDelay == offState){
-        if(multDelay == maxMult){
-          multDelay = 1;
-        }else{
-          multDelay++;
-        } 
-      }     
+
+  if (readingDelay != previousDelay)
+  {
+    if (readingDelay == offState)
+    {
+      if (multDelay == maxMult)
+      {
+        multDelay = 1;
+      }
+      else
+      {
+        multDelay++;
+      }
+    }
   }
   previousDelay = readingDelay;
 }
 
-int switchLight(int greenState,int orangeState,int redState){
-  digitalWrite(orange,orangeState);
-  digitalWrite(red,redState);
-  digitalWrite(green,greenState);
+int switchLight(int greenState, int orangeState, int redState)
+{
+  digitalWrite(orange, orangeState);
+  digitalWrite(red, redState);
+  digitalWrite(green, greenState);
 }
 
 void loop()
 {
   readModeButton();
   readDelayButton();
-  
-  if(numMode == 1){
+
+  if (numMode == 1)
+  {
     digitalWrite(orange, digitalRead(orangeSwitch));
     digitalWrite(green, digitalRead(greenSwitch));
-    digitalWrite(red, digitalRead(redSwitch)); 
+    digitalWrite(red, digitalRead(redSwitch));
   }
-  
-  if(numMode == 2){
-      if (millis() - mode1Time >= (mode1Delay * multDelay)){
-        if(lightOn == 1){
-          switchLight(onState,offState,offState);
-        }
 
-        if(lightOn == 2){
-          switchLight(offState,offState,onState);
-        }
+  if (numMode == 2)
+  {
+    if (millis() - mode1Time >= (mode1Delay * multDelay))
+    {
+      if (lightOn == 1)
+      {
+        switchLight(onState, offState, offState);
+      }
 
-        if(lightOn == 3){
-          switchLight(offState,onState,offState);
-          lightOn = 0;
-        }
-        lightOn++;
-        mode1Time = millis();
-      }  
+      if (lightOn == 2)
+      {
+        switchLight(offState, offState, onState);
+      }
+
+      if (lightOn == 3)
+      {
+        switchLight(offState, onState, offState);
+        lightOn = 0;
+      }
+      lightOn++;
+      mode1Time = millis();
+    }
   }
-  
 
-if(numMode == 3){
-  if (millis() - mode1Time >= (mode1Delay * multDelay)){
-      if(lightOn == 1){
-        switchLight(offState,offState,offState);
+  if (numMode == 3)
+  {
+    if (millis() - mode1Time >= (mode1Delay * multDelay))
+    {
+      if (lightOn == 1)
+      {
+        switchLight(offState, offState, offState);
       }
 
-      if(lightOn == 2){
-        switchLight(onState,offState,offState);
+      if (lightOn == 2)
+      {
+        switchLight(onState, offState, offState);
       }
 
-      if(lightOn == 3){
-        switchLight(onState,onState,offState);
+      if (lightOn == 3)
+      {
+        switchLight(onState, onState, offState);
       }
 
-      if(lightOn == 4){
-        switchLight(onState,onState,onState);
+      if (lightOn == 4)
+      {
+        switchLight(onState, onState, onState);
       }
 
-      if(lightOn == 5){
-        switchLight(offState,onState,onState);
+      if (lightOn == 5)
+      {
+        switchLight(offState, onState, onState);
       }
 
-      if(lightOn == 6){
-        switchLight(offState,offState,onState);
+      if (lightOn == 6)
+      {
+        switchLight(offState, offState, onState);
         lightOn = 0;
       }
 
       lightOn++;
       mode1Time = millis();
-    } 
-  } 
+    }
+  }
 }
